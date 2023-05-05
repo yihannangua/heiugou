ac18b4b10c7622faba32422fb99451ab1123fcd4
yihannangua <54133432+yihannangua@users.noreply.github.com>
2023-05-05 14:35:16 +0800

    更新代码

--- request/request.js
+++ request/request.js
@@ -4,7 +4,7 @@ export const request = (params) => {
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve,reject) => {
         wx.request({
-            ...params,
+            // ...params,通过下方拼接方法，则不再需要展开
             url:baseUrl+params.url,
             success:(res) => {
                 resolve(res);
