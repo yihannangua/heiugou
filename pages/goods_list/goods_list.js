dda9ad200a3310b82e71ca4778c06b5271867833
yihannangua <54133432+yihannangua@users.noreply.github.com>
2023-05-08 10:11:04 +0800

    更新代码

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -24,7 +24,7 @@ Page({
         isActive: false
       }
     ],
-    goodsList:{}
+    goodsList:[]
   },
   //接口需要的参数
   QueryParams: {
@@ -33,6 +33,8 @@ Page({
     pagenum:1,
     pagesize:10
   },
+  //总商品页数
+  totalPages:1,
 
   /**
    * 生命周期函数--监听页面加载
@@ -41,13 +43,47 @@ Page({
     this.QueryParams.cid = options.cid;
     this.getGoodsList();
   },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+    //判断是否有下一页数据
+    if (this.QueryParams.pagenum >= this.totalPages) {
+      wx.showToast({
+        title: '数据到底了',
+        duration: 1500,
+      });
+        
+    } else {
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+  /*
+  页面下拉刷新时间的处理函数 
+  */
+  onPullDownRefresh() {
+    // 下拉刷新的逻辑
+    this.setData({
+      goodsList: []
+    });
+    this.QueryParams.pagenum = 1;
+    this.getGoodsList();
+    // 可以通过调用接口、重新渲染页面等方式实现数据的动态更新
+    wx.showToast({
+      title: '数据已更新'
+    })
+    // 停止下拉刷新动画
+    wx.stopPullDownRefresh()
+  },
   //定义获取商品列表数据的方法
   async getGoodsList() {
     const res = await request({url:"/goods/search",data:this.QueryParams});
-    console.log(res);
+    this.totalPages = Math.ceil(res.data.message.total / this.QueryParams.pagesize)
+    // console.log(res.data.message.goods);
     this.setData({
-      goodsList:res.data.message.goods
-    })
+      goodsList: [...this.data.goodsList,...res.data.message.goods]
+    });
   },
   //定义标题点击事件 从子组件传递过来
   handleTabsItemChange(e) {
--- request/request.js
+++ request/request.js
@@ -1,5 +1,17 @@
+
+//同时发送异步请求的次数
+let ajaxTimes = 0;
+
 // 封装网络请求函数并导出
 export const request = (params) => {
+    ajaxTimes++;
+    //显示加载图标
+    wx.showLoading({
+        title: "加载中",
+        mask: true
+    });
+
+
     //定义公共的url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve,reject) => {
@@ -12,6 +24,13 @@ export const request = (params) => {
             },
             fail:(err) => {
                 reject(err);
+            },
+            complete:() => {
+                ajaxTimes--;
+                //所有异步请求都执行完毕，关闭加载中图标
+                if (ajaxTimes===0) {
+                    wx.hideLoading();
+                }
             }
         });
     })
